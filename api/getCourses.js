57a9e0c26274b5629a7791c01e156e1dc2908221
jian zhou <jianzhou@MacBook-Pro-di-jian.local>
2019-10-16 12:00:43 +0200

    api refactor

--- api/getCourses.js
+++ api/getCourses.js
@@ -11,55 +11,25 @@ const getCourseList = (parsetBody) => {
     return parsetBody.child[0].child[3].child[3].child[2].child[0].child[3].child[3].child[6].child[3].child
 }
 const getCourseInfo = (courseObj) => {
-    const resDataFormatted = [];
-    /*
-        Dati di interesse: 
-            -   titolo [0]
-            -   tag [1]
-            -   lingua [2]
-            -   sede [3]
-            -   iscrizioni [4]
-            -   more info [5]
-    */
+    // Extract data
     const infoList = (courseObj.child.map((el) => { return el.child })).map(el => {
         return el.map(el2 => { return el2 })
     })
-    // titolo [0]
-    resDataFormatted.push({
-        key: "title",
-        value: ((infoList[0][0].child[0].text).split("&#39;")).join("'")
-    });
-    // tag [1]
-    resDataFormatted.push({
-        key: "tag",
-        value: infoList[1][1].text
-    });
-    // lingua [2]
-    resDataFormatted.push({
-        key: "language",
-        value: infoList[2][1].text
-    });
-    // sede [3]
-    resDataFormatted.push({
-        key: "location",
-        value: ((infoList[3][1].text).split("&agrave;")).join("à ")
-    });
-    // iscrizioni [4]
-    resDataFormatted.push({
-        key: "subscription start",
-        value: infoList[4][1].child[0].text
-    });
-    // iscrizioni [4]
-    resDataFormatted.push({
-        key: "subscription end",
-        value: infoList[4][2].child[0].text
-    });
-    // more info [5]
-    resDataFormatted.push({
-        key: "more info",
-        value: ((infoList[infoList.length - 1][0].attr.href).split("amp;")).join("")
-    });
-    return resDataFormatted
+    // Return formatted data
+    /**
+     * TODO
+     *  - title, se troppo lungo vengono dati puntini puntini quindi va fatta la richiesta al link per avere il nome completo
+     */
+    return {
+        title: ((infoList[0][0].child[0].text).split("&#39;")).join("'"),
+        tag: infoList[1][1].text,
+        lang: infoList[2][1].text,
+        where: ((infoList[3][1].text).split("&agrave;")).join("à "),
+        subStartDate: ((infoList[4][1].child[0].text).split("dal ")).join(""),
+        subEndDate: ((infoList[4][2].child[0].text).split("del "))[1],
+        subEndTime: ((((infoList[4][2].child[0].text).split(" del"))[0]).split("alle ore "))[1],
+        link: ((infoList[infoList.length - 1][0].attr.href).split("amp;")).join("")
+    };
 }
 module.exports = (req, res) => {
     const getData = https.request(options, resData => {
