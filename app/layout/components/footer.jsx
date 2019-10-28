4454ebf0f34a7a44a09991114de86c836245d010
jian zhou <jianzhou@MacBook-Pro-di-jian.local>
2019-10-28 17:03:18 +0100

    version 0.1.1

--- app/layout/components/footer.jsx
+++ app/layout/components/footer.jsx
@@ -6,7 +6,18 @@ const Footer = () => (
     style={{ backgroundColor: "#0f2c53" }}
     className="text-center text-light p-3"
   >
-    <p>Alternative website of...</p>
+    <p>
+      This is a fake website of{" "}
+      <a
+        className="text-light"
+        href="https://www.polimi.it/corsi/passion-in-action/"
+        rel="nofollow"
+      >
+        Polimi's Passion In Action Official website.
+      </a>
+    </p>
+
+    <p>Version 0.1.1</p>
     <p className="m-0">
       {" "}
       ©{new Date().getFullYear()}{" "}
