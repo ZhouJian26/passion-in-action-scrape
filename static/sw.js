5e56137437a417ee69b8ece26c0e33bba2fe7ee4
jian zhou <jianzhou@MacBook-Pro-di-jian.local>
2019-11-06 09:30:23 +0100

    pwa implementation 1.0.0

---
+++ static/sw.js
@@ -0,0 +1,43 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+if (workbox) {
+  workbox.routing.registerRoute(
+    "/api/dataFetch.js",
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "data-course",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
+  );
+  workbox.routing.registerRoute(
+    "/",
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "app",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
+  );
+  workbox.routing.registerRoute(
+    /static/,
+    new workbox.strategies.CacheFirst({
+      cacheName: "app-static-file",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 32,
+          maxAgeSeconds: 60 * 60 * 24 * 365 // 365 giorni
+        }),
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
+  );
+}
