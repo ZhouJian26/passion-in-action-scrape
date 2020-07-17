5fe05e36b582e90d6699401dc09aa3446186263d
Zhou Jian <jian.zhou@mail.polimi.it>
2020-07-17 17:15:11 +0200

    rename package name

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -1,6 +1,6 @@
 "use strict";
 const request = require("request");
-/** mask */
+
 module.exports = (req, res) => {
   request.get(
     `https://test-lamda-load.s3.eu-central-1.amazonaws.com/data.json`,
