75c3140f4af9e808c3bdd6e8739dc2c30d678327
Zhou Jian <jian.zhou@mail.polimi.it>
2020-07-18 12:14:58 +0200

    use env

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -1,11 +1,22 @@
 "use strict";
-const request = require("request");
 
+require("dotenv").config();
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3({
+  region: process.env.AWS_S3_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+const params = {
+  Bucket: process.env.AWS_S3_BUCKET_NAME,
+  Key: process.env.AWS_S3_KEY_FILE_NAME,
+};
 module.exports = (req, res) => {
-  request.get(
-    `https://test-lamda-load.s3.eu-central-1.amazonaws.com/data.json`,
-    async (error, response, data) => {
-      res.send(data);
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      res.send([]);
+      return;
     }
-  );
+    res.send(data.Body.toString());
+  });
 };
